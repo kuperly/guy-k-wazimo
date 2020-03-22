34fedc246e1d8805f532fc95fc5957c1333fc334
guy kuperly <guyk22@gmail.com>
2020-03-22 20:37:27 +0200

    add proxy.conf.json support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {catchError, finalize, mergeMap} from 'rxjs/operators';
-import {of, throwError} from 'rxjs';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { catchError, finalize, mergeMap } from "rxjs/operators";
+import { of, throwError } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
   public tableData;
@@ -14,44 +14,51 @@ export class AppComponent implements OnInit {
   public loading = false;
 
   public filters = {
-    first_name: '',
-    last_name: '',
-   email: '',
-   gender: ''
-  }
-  public fnModule = '';
-  public lnModule = '';
-  public emailModule = '';
-  public genderModule = '';
+    first_name: "",
+    last_name: "",
+    email: "",
+    gender: ""
+  };
+  public fnModule = "";
+  public lnModule = "";
+  public emailModule = "";
+  public genderModule = "";
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.loading = true;
-    // this.http.get( 'https://storage.googleapis.com/static.aoni.io/demo/user.json', {headers: {'Access-Control-Allow-Origin': '*'}})
-    this.http.get( 'assets/User.json')
+    this.http
+      .get("/static.aoni.io/demo/user.json")
       .pipe(
-        mergeMap((res) => {
+        mergeMap(res => {
           this.tableData = this.tableDataFiltered = res;
           return of(res);
         }),
-        catchError((err) => {
+        catchError(err => {
           return throwError(err);
         }),
         finalize(() => {
           this.loading = false;
         })
-      ).subscribe((res) => {
-    });
+      )
+      .subscribe(res => {});
   }
 
   onColFilterChanged() {
-    this.tableDataFiltered = this.tableData.filter((row) => {
-      return (!this.filters.first_name || row['first_name'].toLowerCase().indexOf(this.filters.first_name) !== -1) &&
-        (!this.filters.last_name || row['last_name'].toLowerCase().indexOf(this.filters.last_name) !== -1) &&
-        (!this.filters.email || row['email'].toLowerCase().indexOf(this.filters.email) !== -1) &&
-        (!this.filters.gender || row['gender'].toLowerCase().indexOf(this.filters.gender) !== -1);
+    this.tableDataFiltered = this.tableData.filter(row => {
+      return (
+        (!this.filters.first_name ||
+          row["first_name"].toLowerCase().indexOf(this.filters.first_name) !==
+            -1) &&
+        (!this.filters.last_name ||
+          row["last_name"].toLowerCase().indexOf(this.filters.last_name) !==
+            -1) &&
+        (!this.filters.email ||
+          row["email"].toLowerCase().indexOf(this.filters.email) !== -1) &&
+        (!this.filters.gender ||
+          row["gender"].toLowerCase().indexOf(this.filters.gender) !== -1)
+      );
     });
   }
 }
